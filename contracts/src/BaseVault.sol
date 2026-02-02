6e64aae7d6fca62bfc81ce52b38b720dd81135bb
Sahib-Aujla <sahibpreet568@gmail.com>
2026-02-02 16:57:44 -0400

    added baseVault

---
+++ contracts/src/BaseVault.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BaseVault is ERC4626, Ownable {
+    constructor(IERC20 _asset) ERC20("Tranche Vault Share", "TVS") ERC4626(_asset) Ownable(msg.sender) {}
+}
